20dd575688fd2a74e851ed64b8fbe734c73ba75f
BrazilPepe <labancz.marton@gmail.com>
2017-03-17 12:11:53 +0100

    Vége

--- 2F-ZH1/Program.cs
+++ 2F-ZH1/Program.cs
@@ -14,9 +14,12 @@ namespace _2F_ZH1
             Verseny V1 = new Verseny();
             {
                 Csiga Csiga = new Csiga( 1, 50);
-
-
-               
+                Hóvirág h = new Hóvirág();
+                Medve m = new Medve();
+                TréMedve tm = new TréMedve();
+                Róka r = new Róka();
+                Szöcske sz = new Szöcske();
+                Nyúl ny = new Nyúl();
             }
             
             Console.ReadKey();
